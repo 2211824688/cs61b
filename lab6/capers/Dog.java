0d04fb20489e86299bbb04784498d7c1878fbb49
2211824688 <2211824688@qq.com>
2024-04-12 17:02:52 +0800

    finish lab6

--- lab6/capers/Dog.java
+++ lab6/capers/Dog.java
@@ -1,17 +1,18 @@
 package capers;
 
 import java.io.File;
+import java.io.IOException;
 import java.io.Serializable;
 import static capers.Utils.*;
 
 /** Represents a dog that can be serialized.
- * @author TODO
+ * @author yangHaoChen
 */
-public class Dog { // TODO
+public class Dog implements Serializable {
 
     /** Folder that dogs live in. */
-    static final File DOG_FOLDER = null; // TODO (hint: look at the `join`
-                                         //      function in Utils)
+    static final File DOG_FOLDER = Utils.join(CapersRepository.CAPERS_FOLDER, "dogs");
+
 
     /** Age of dog. */
     private int age;
@@ -39,8 +40,7 @@ public class Dog { // TODO
      * @return Dog read from file
      */
     public static Dog fromFile(String name) {
-        // TODO (hint: look at the Utils file)
-        return null;
+        return Utils.readObject(Utils.join(DOG_FOLDER, name), Dog.class);
     }
 
     /**
@@ -55,8 +55,11 @@ public class Dog { // TODO
     /**
      * Saves a dog to a file for future use.
      */
-    public void saveDog() {
-        // TODO (hint: don't forget dog names are unique)
+    public void saveDog() throws IOException {
+        File dog = Utils.join(DOG_FOLDER, this.name);
+        if (dog.createNewFile()) {
+            Utils.writeObject(dog, this);
+        }
     }
 
     @Override
@@ -65,5 +68,4 @@ public class Dog { // TODO
             "Woof! My name is %s and I am a %s! I am %d years old! Woof!",
             name, breed, age);
     }
-
 }
